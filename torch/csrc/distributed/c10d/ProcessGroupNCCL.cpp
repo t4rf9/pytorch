f419c9b3ad377fa74d4ac9a39609bec8c6f6b2a3
Will Constable <whc@meta.com>
2024-02-28 13:04:21 -0800

    [C10D] Fix coalesing_manager coalesced op Flight Recording
    The coalescing manager api works by accumulating operations in python
    via a contextmanager, and then making one call into c++ to an <op>_coalesced
    API.  It has limited support for ops and has been added recently to
    avoid overheads of making individual py-cpp calls.
    
    This complicates flight recording..
    
    For now, flight recording of coalescing_manager collectives is less
    detailed than cpp coalesced collectives.
    
    TODO test the other 2 ops that are supported by coalescing_manager
    
    ghstack-source-id: ddb126a920c607bef8c1ac02af6b2771227c9b7f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120430

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -2519,7 +2519,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collectiveCoalesced(
   syncStream(device, ncclEvents_[key], ncclStream);
 
   auto work = initWork(
-      device, rank_, opType, nullptr, inputs, outputs, /*record=*/true);
+      device, rank_, opType, profilingTitle, inputs, outputs, /*record=*/true);
 
   // Store references to outputs to be used by WorkNCCL::result and operator<<.
   work->outputs_ = std::make_shared<std::vector<at::Tensor>>(outputs);
@@ -2531,7 +2531,8 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collectiveCoalesced(
 
   at::cuda::OptionalCUDAGuard gpuGuard;
 
-  // Start event should only be recorded before the ncclGroupStart()
+  // Start event should only be recorded before the ncclGroupStart() (which
+  // happens inside AutoNcclGroup guard below)
   if (work->timingEnabled_) {
     work->ncclStartEvent_->record(ncclStream);
   }
@@ -2588,10 +2589,7 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collectiveCoalesced(
     }
   }
 
-  // End event should only be recorded after the ncclGroupEnd()
-  if (!coalescing_state_) {
-    work->ncclEndEvent_->record(ncclStream);
-  }
+  work->ncclEndEvent_->record(ncclStream);
   work->ncclComm_ = ncclComm;
 
   {
@@ -2629,11 +2627,10 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collectiveCoalesced(
   // Notify graphs before we check the capture status preemptively
   at::cuda::CUDAGraph::inc_pending_event_queries();
 
-  if (!coalescing_state_ && capture_status == c10::cuda::CaptureStatus::None) {
-    workEnqueue(work);
-  } else {
-    at::cuda::CUDAGraph::dec_pending_event_queries();
-  }
+  workEnqueue(work);
+  // TODO(whc) did I delete a relevant codepath here?
+  // at::cuda::CUDAGraph::dec_pending_event_queries();
+  // }
 
   return work;
 }
