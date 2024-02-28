06230654e3a0ff616da5a6c08b74dcd4086ad537
Simon Fan <xmfan@meta.com>
2024-02-28 09:21:08 -0800

    Move VariableInfo into its own file to avoid circular dependency
    ghstack-source-id: e7fdf533e1b6c3c6793066c358e886c0b51513ef
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/120732

---
+++ torch/csrc/autograd/variable_info.cpp
@@ -0,0 +1,27 @@
+#include <ATen/ops/zeros.h>
+#include <torch/csrc/autograd/variable.h>
+#include <torch/csrc/autograd/variable_info.h>
+
+namespace torch::autograd {
+
+VariableInfo::VariableInfo(const Variable& var)
+    : layout(var.layout()),
+      device(var.device()),
+      scalar_type(var.scalar_type()),
+      size(var.sym_sizes().vec()),
+      requires_grad(var.requires_grad()),
+      is_empty(false) {}
+
+VariableInfo::VariableInfo() : requires_grad(false), is_empty(true) {}
+
+Variable VariableInfo::zeros(at::OptionalDeviceGuard& device_guard) const {
+  if (is_empty) {
+    // Return undefined tensor.
+    return at::Tensor();
+  } else {
+    return at::zeros_symint(
+        size, at::TensorOptions(scalar_type).device(device).layout(layout));
+  }
+}
+
+} // namespace torch::autograd
